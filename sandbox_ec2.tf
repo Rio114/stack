0974c15325c37b846ce484870aac87f7a31903b6
Rio114 <nomuraryoji114@gmail.com>
2021-10-31 12:03:36 +0900

    misc

--- sandbox_ec2.tf
+++ sandbox_ec2.tf
@@ -1,12 +1,12 @@
 provider "aws"{
-    region = "us-east-1"
+    region = "ap-northeast-1"
 }
 
 resource "aws_instance" "sandbox" {
-  count         = 2
-  ami           = "ami-0ff8a91507f77f867"
+  count         = 1
+  ami           = "ami-06cd52961ce9f0d85"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-00519e21"
+  subnet_id     = "subnet-0a39d7cfb15f41cc2"
 
   tags = {
     Name = "${format("sandbox-%02d", count.index + 1)}"
